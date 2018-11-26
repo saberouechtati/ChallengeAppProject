e304d193772c08580408f4cf1809534a68f2e5da
Saber Ouechtati <saberouechtati@gmail.com>
2018-11-26 02:15:20 +0100

    Create a sample RecyclerView, RecyclerViewAdapter to display the GitHub repository list.

--- app/src/main/java/saber/com/challengeappproject/ui/repositorylist/RepositoryListFragment.java
+++ app/src/main/java/saber/com/challengeappproject/ui/repositorylist/RepositoryListFragment.java
@@ -5,14 +5,24 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import saber.com.challengeappproject.R;
+import saber.com.challengeappproject.adapters.RecyclerViewAdapter;
 import saber.com.challengeappproject.viewmodels.RepositoryListViewModel;
 
 public class RepositoryListFragment extends Fragment {
+
+    private static final String TAG = "RepositoryListFragment";
+
+    protected RecyclerView mRecyclerView;
+    protected RecyclerViewAdapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    protected String[] mDataset;
 
     private RepositoryListViewModel mViewModel;
 
@@ -24,7 +34,31 @@ public class RepositoryListFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.repository_list_fragment, container, false);
+        View rootView = inflater.inflate(R.layout.repository_list_fragment, container, false);
+        rootView.setTag(TAG);
+
+        mViewModel = ViewModelProviders.of(this).get(RepositoryListViewModel.class);
+        // TODO: Use the ViewModel
+        mDataset = mViewModel.getRepositoryList(50);
+
+        // BEGIN_INCLUDE(initializeRecyclerView)
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerView);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(rootView.getContext());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+
+        mAdapter = new RecyclerViewAdapter(mDataset);
+        // Set CustomAdapter as the adapter for RecyclerView.
+        mRecyclerView.setAdapter(mAdapter);
+        // END_INCLUDE(initializeRecyclerView)
+
+        return rootView;
     }
 
     @Override
@@ -32,6 +66,7 @@ public class RepositoryListFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(this).get(RepositoryListViewModel.class);
         // TODO: Use the ViewModel
+        mDataset = mViewModel.getRepositoryList(50);
     }
 
 }
--- app/src/main/java/saber/com/challengeappproject/viewmodels/RepositoryListViewModel.java
+++ app/src/main/java/saber/com/challengeappproject/viewmodels/RepositoryListViewModel.java
@@ -4,4 +4,12 @@ import android.arch.lifecycle.ViewModel;
 
 public class RepositoryListViewModel extends ViewModel {
     // TODO: Implement the ViewModel
+    public String[] getRepositoryList(int count) {
+        String[] mDataset;
+        mDataset = new String[count];
+        for (int i = 0; i < count; i++) {
+            mDataset[i] = "This is element #" + i;
+        }
+        return mDataset;
+    }
 }
